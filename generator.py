1b63486e23bf4630edee0ba1cc1e48f331b01ffb
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-15 14:10:48 +0200

    Add files via upload

--- generator.py
+++ generator.py
@@ -2,25 +2,33 @@ import random
 
 
 def main():
-    get_level()
+    lvl= get_level()
+    x,y = generate_integer(lvl)
+    print(f"{x}+{y}")
 
 
 def get_level():
-    lvl= input("Level")
     while True:
-        if lvl == "1":
-            x = random.randrange(10)
-            y= random. randrange(10)
-            print(x,"+",y,"=",  )
-            if
-
+        lvl= input("Level")
+        if lvl == "1" or lvl== "2" or lvl== "3":
+            return int(lvl)
+        else:
+            continue
 
 
 
 
 def generate_integer(level):
-    ...
-
+    if level == 1:
+        x = random.randrange(10)
+        y = random.randrange(10)
+    if level == 2:
+        x = random.randrange(100)
+        y = random.randrange(100)
+    if level == 3:
+        x = random.randrange(1000)
+        y = random.randrange(1000)
+    return x, y
 
 if __name__ == "__main__":
     main()
