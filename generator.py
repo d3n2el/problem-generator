a1923cdc8250aa7641809fa772d9cc0b622279a5
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-15 20:51:33 +0200

    Add files via upload

--- generator.py
+++ generator.py
@@ -1,17 +1,17 @@
 import random
 
-
 def main():
     error= 0
+    err= 0
     attempts= 10
     lvl= get_level()
     p= 10
-    w=3
     while p> 0:
-        x,y = generate_integer(lvl)
+        x= generate_integer(lvl)
+        y= generate_integer(lvl)
         i=3
         while i >0:
-            print(f"{x}+{y}=", end=" ")
+            print(f"{x} + {y} =", end=" ")
             m = int(input())
             i= i-1
             p= p-1
@@ -19,10 +19,14 @@ def main():
                 break
             if not m == x + y:
                 error+= 1
-                print("EEE")`
-            print(x+y)
+                print("EEE")
+        if not m== x+y:
+            err+=1
+        if error== 3:
+            print(f"{x} + {y} = {q}")
+            error= 0
 
-    f= attempts-error
+    f= attempts-err
     print("Score:",f)
 def get_level():
     while True:
@@ -33,19 +37,20 @@ def get_level():
             continue
 
 
-
-
 def generate_integer(level):
-    if level == 1:
-        x = random.randrange(10)
-        y = random.randrange(10)
-    if level == 2:
-        x = random.randrange(100)
-        y = random.randrange(100)
-    if level == 3:
-        x = random.randrange(1000)
-        y = random.randrange(1000)
-    return x, y
+    match level:
+        case 1:
+            n= random.randint(0,9)
+            return n
+        case 2:
+            n= random.randint(0,99)
+            return n
+        case 3:
+            n= random.randint(0,999)
+            return n
+        case _:
+            raise ValueError
 
 if __name__ == "__main__":
     main()
+
