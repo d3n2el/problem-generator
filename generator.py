81656333b6f6138bbb39888becb26d0c099a9176
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-15 13:58:43 +0200

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,26 @@
+import random
+
+
+def main():
+    get_level()
+
+
+def get_level():
+    lvl= input("Level")
+    while True:
+        if lvl == "1":
+            x = random.randrange(10)
+            y= random. randrange(10)
+            print(x,"+",y,"=",  )
+            if
+
+
+
+
+
+def generate_integer(level):
+    ...
+
+
+if __name__ == "__main__":
+    main()
